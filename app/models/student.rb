bd22d4f003c092ca6007cc077891ee07a85d7277
funnymo <mky.wee@gmail.com>
2016-04-13 16:29:13 +0800

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,22 @@
 require_relative '../../db/config'
+require 'byebug'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+attr_accessor :name, :first_name, :last_name, :gender, :birthday
+	validates :email, uniqueness: true, format: { with: /\w+@\w+\.\w{2,}/, message: 'must be valid email' }
+	validates :age, numericality: { greater_than: 4 }
+	validates :phone, format: { with: /.*\d{3}.*\d{3}.*\d{4}.*/, message: 'must be at least 10 digit phone number' }
+
+	def name
+		@student = Student.new
+		@student.name = "#{first_name} #{last_name}"
+	end
+
+	def age
+		# byebug
+		now = Date.today
+		age = now.year - birthday.year - 1
+	end
 end
